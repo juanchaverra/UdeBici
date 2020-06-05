2e889ba43c5cb3121046ab7426f8ab7e155759f1
davilo12 <david.bedoya@idata.com.co>
2020-06-04 20:45:38 -0500

    Conexión base de datos funcional

--- baseDatos.py
+++ baseDatos.py
@@ -1,14 +1,11 @@
 import pyodbc
-#import pandas as pd
+import pandas as pd
 
-# Query Data completa
-
-# Query Data completa
 server = 'servidorrpueba.database.windows.net'
 database = 'Bicicletas'
 username = 'adminudea'
 password = 'udea2020**'
-driver = '{ODBC Driver 19 for SQL Server}'
+driver = '{ODBC Driver 17 for SQL Server}'
 cnxn = pyodbc.connect(
    'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 
@@ -19,24 +16,5 @@ Query = ("SELECT * "
 
 sql = Query
 
-#df_complete = pd.read_sql(sql, cnxn)
-
-
-
-
-
-
-"""
-conn = pyodbc.connect(
-    'DRIVER=FreeTDS;SERVER=servidorrpueba.database.windows.net;PORT=1433;UID=adminudea;PWD=udea2020**;DATABASE=Bicicletas;UseNTLMv2=yes;TDS_Version=8.0')
-
-
-cursor = conn.cursor()
-
-cursor.execute("SELECT * FROM BiciUsuarios")
-rows = cursor.fetchall()
-
-if rows:
-    print(rows)
-
-"""
+df_complete = pd.read_sql(sql, cnxn)
+print(df_complete)
